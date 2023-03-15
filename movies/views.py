cc2749e73b0f040f0fe4e6148daca4830f5ddba8
eddy2024 <eddysoftwarengineer@gmail.com>
2023-03-15 18:22:53 +0000

    m17 passing 404

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -11,10 +11,12 @@ def index(request):
    
    
 def detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+    movie = get_object_or_404(Movie,pk=movie_id)
     return render(request, 'movies/detail.html', {'movie': movie})
-    
-    
+        
+   
+      
+        
     
     #return HttpResponse(movie_id)
    
